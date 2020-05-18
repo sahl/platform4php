3287f68c571b375b362b2cac0417423e7cacc43a
Michael Sahl <ms@ventu.dk>
2020-05-18 08:13:19 +0200

    More new component stuff

--- Platform/Component/php/get_content.php
+++ Platform/Component/php/get_content.php
@@ -4,9 +4,8 @@ include $_SERVER['DOCUMENT_ROOT'].'/Platform/include.php';
 
 $class = $_POST['__class'];
 
-if (!class_exists($class)) die('Invalid component class');
-
-$component = new $class($_POST);
+$component = unserialize(base64_decode($_POST['object']));
+if (! $component instanceof \Platform\Component) die('Invalid component class');
 $component->dontLoadScript();
 
 if ($component->is_secure && !Accesstoken::validateSession()) die('Must be logged in');
