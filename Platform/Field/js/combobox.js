655587657d2ac5b6a9750fc4e43acc84a15a6303
Michael Sahl <ms@ventu.dk>
2020-11-11 15:04:25 +0100

    Split combobox input field into three different field types and made better inheritage (datarecordcombobox and combobox isn't tested yet)

--- Platform/Field/js/combobox.js
+++ Platform/Field/js/combobox.js
@@ -5,13 +5,15 @@ addCustomPlatformFunction(function(item) {
             source: element.data('source'),
             minLength: 2,
             select: function(event, ui) {
-                if (element.hasClass('platform_datarecord_combobox')) {
+                if (element.hasClass('platform_indexed_combobox')) {
                     element.prev().val(ui.item.real_id);
                     element.data('validated_value', ui.item.value);
                 }
             },
             change: function(event, ui) {
-                if (element.val() != element.data('validated_value')) $(this).prev().val('0');
+                if (element.hasClass('platform_indexed_combobox')) {
+                    if (element.val() != element.data('validated_value')) $(this).prev().val('0');
+                }
             }
         });
         return true;
