586e8021b2a9585f57e283b02fce71cdde9fc125
Michael Sahl <ms@ventu.dk>
2020-11-11 15:00:36 +0100

    The API can now handle custom requests and handle file and image fields

---
+++ Platform/Api/Client.php
@@ -0,0 +1,104 @@
+<?php
+namespace Platform;
+
+class ApiClient {
+    
+    protected $endpoint = false;
+    
+    protected $token_code = '';
+    
+    public function __construct($endpoint) {
+        $this->endpoint = $endpoint;
+    }
+    
+    /**
+     * Parse a RAW HTTP response
+     * @param string $http_output HTTP response
+     * @return array Hashed by code=http code  message=http message  headers=array of all headers
+     * body=body output  json=json decoded body output
+     */
+    public static function parseResponse($http_output) {
+        $lines = explode("\n", $http_output);
+        $parsingheader = true; $canswitch = false;
+        $result = array();
+        foreach ($lines as $line) {
+            $line = trim($line);
+            if ($line == '') {
+                if ($parsingheader && $canswitch) $parsingheader = false;
+                continue;
+            }
+            if ($parsingheader) {
+                if (preg_match('/^HTTP\\/\\S* (\\d+) (.*)$/', $line, $match)) {
+                    $result['code'] = $match[1];
+                    $result['message'] = $match[2];
+                    continue;
+                }
+                if (strpos($line, ':') !== false) {
+                    $canswitch = true;
+                    $keyword = substr($line,0,strpos($line,':'));
+                    $value = substr($line, strpos($line,':')+1);
+                    $result['headers'][$keyword] = $value;
+                }
+            } else {
+                $result['body'] .= $line;
+            }
+        }
+        $result['json'] = json_decode($result['body'], true);
+        return $result;
+    }
+    
+    /**
+     * Query the API
+     * @param string $object The object to query
+     * @param string $method The method to use
+     * @param int $id The object ID to query (if any)
+     * @param array $parameters Parameters for GET or POST as hashed array
+     * @return array Hashed by code=http code  message=http message  headers=array of all headers
+     * body=body output  json=json decoded body output
+     */
+    public function query($object, $method = 'GET', $id = 0, $parameters = array()) {
+        $endpoint = $this->endpoint;
+        if (substr($endpoint,-1,1) != '/') $endpoint .= '/';
+        $endpoint .= $object;
+        if ($id) $endpoint .= '/'.$id;
+        if (strtolower($method) == 'get' && count($parameters)) {
+            // Build querystring
+            $endpoint .= '?'.http_build_query($parameters);
+        }
+        
+        // Prepare CURL
+        var_dump($endpoint);
+        $curl = curl_init($endpoint);
+        $options = array(
+            'Access-Token: ' . 'xxx',
+            'Content-Type: application/json',
+            'Accept: application/json'
+        );
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $options);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+        if ($this->token_code) {
+            curl_setopt($curl, CURLOPT_COOKIE, 'access_token='.$this->token_code.'; path:/;');
+        }
+
+        if ($method == 'POST' || $method == 'PUT') {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($parameters));
+        }
+
+        $curlResponse = curl_exec($curl);
+        curl_close($curl);
+
+        return $this->parseResponse($curlResponse);
+    }
+    
+    /**
+     * Set the access token to use for logging in with the API
+     * @param type $token_code
+     */
+    public function setAccessToken($token_code) {
+        $this->token_code = $token_code;
+    }
+    
+}
--- Platform/Api/api.php
+++ Platform/Api/api.php
@@ -28,16 +28,41 @@ class Api {
     }
 
     /**
+     * Override this to implement a custom handler after any security check
+     * @param string $object_name Object name for custom handler
+     * @param int $object_id Object ID for custom handler
+     * @param string $method Method for custom handler. GET POST or DELETE
+     * @param string $get Get input for custom handler
+     * @param string $body Body input for custom handler
+     */
+    public function customHandlerAfterSecurity($object_name, $object_id, $method, $get, $body) {
+        
+    }
+    
+    /**
+     * Override this to implement a custom handler before any security check
+     * @param string $object_name Object name for custom handler
+     * @param int $object_id Object ID for custom handler
+     * @param string $method Method for custom handler. GET POST or DELETE
+     * @param string $get Get input for custom handler
+     * @param string $body Body input for custom handler
+     */
+    public function customHandlerBeforeSecurity($object_name, $object_id, $method, $get, $body) {
+        
+    }
+
+    /**
      * Convert a Datarecord object to an API object
      * @param String $class A Datarecord class name
      * @param Datarecord $object An object of the given type
+     * @param Boolean $retrieve_binary_data If true, then pass file and image fields as binary data
      * @return array An API object expressed as an array
      */
-    private static function getApiObject($class, $object) {
+    private static function getApiObject($class, $object, $retrieve_binary_data = false) {
         Errorhandler::checkParams($class, 'string', $object, '\\Platform\\Datarecord');
         $result = array();
         foreach ($class::getStructure() as $key => $definition) {
-            if ($definition['invisible'] &&  $definition['fieldtype'] != Datarecord::FIELDTYPE_KEY) continue;
+            if ($definition['invisible'] && $definition['fieldtype'] != Datarecord::FIELDTYPE_KEY) continue;
             switch ($definition['fieldtype']) {
                 case Datarecord::FIELDTYPE_PASSWORD:
                     continue;
@@ -47,6 +72,24 @@ class Api {
                 case Datarecord::FIELDTYPE_DATE:
                 case Datarecord::FIELDTYPE_DATETIME:
                     $result[$key] = $object->getRawValue($key)->get();
+                    break;
+                case Datarecord::FIELDTYPE_IMAGE:
+                case Datarecord::FIELDTYPE_FILE:
+                    if ($retrieve_binary_data) {
+                        $file = new File();
+                        $file->loadForRead($object->getRawValue($key));
+                        if ($file->isInDatabase() && $file->canAccess()) {
+                            $result[$key] = array(
+                                'filename' => $file->filename,
+                                'mimetype' => $file->mimetype,
+                                'binary' => base64_encode($file->getFileContent())
+                            );
+                        } else {
+                            $result[$key] = null;
+                        }
+                    } else {
+                        $result[$key] = $object->getRawValue($key);
+                    }
                     break;
                 default:
                     $result[$key] = $object->getRawValue($key);
@@ -80,6 +123,12 @@ class Api {
         $instance->loadForRead($instance_id);
         if (! $instance->isInDatabase()) self::respondAndDie (404, 'No such instance: '.$instance_id);
         $instance->activate();
+
+        // Get input
+        $input = file_get_contents("php://input");
+        $method = strtoupper($_SERVER['REQUEST_METHOD']);
+        
+        $this->customHandlerBeforeSecurity($object_name, $object_id, $method, $_GET, $input);
         
         // Check for valid access
         $token_code = $_COOKIE['access_token'];
@@ -87,13 +136,14 @@ class Api {
         if (! $token_code) self::respondAndDie (401, 'No access token provided');
         if (!Accesstoken::validateTokenCode($token_code)) self::respondAndDie (401, 'Invalid or expired access token');
         
+        $this->customHandlerAfterSecurity($object_name, $object_id, $method, $_GET, $input);
+        
         // Check for valid object
         if (! isset($this->classes[$object_name])) self::respondAndDie (404, 'No such object type: '.$object_name);
         $class = $this->classes[$object_name];
         
-        switch ($_SERVER['REQUEST_METHOD']) {
+        switch ($method) {
             case 'POST':
-                $input = file_get_contents("php://input");
                 if (! $input) self::respondAndDie(400, 'No post data received');
                 $json = json_decode($input, true);
                 if ($json === null) self::respondAndDie(400, 'Data was received, but wasn\'t valid json');
@@ -124,10 +174,10 @@ class Api {
                     $object->loadForRead($object_id);
                     if (! $object->isInDatabase()) self::respondAndDie(404, 'No object of type: '.$object_name.' with id: '.$object_id);
                     if (! $object->canAccess()) self::respondAndDie(403, 'You don\'t have the permission to access this object.');
-                    $response = self::getApiObject($class, $object);
+                    $response = self::getApiObject($class, $object, $_GET['include_binary_data'] == 1);
                     self::respondAndDie(200, json_encode($response));
                 } else {
-                    $filter = new Filter($class);
+                    $filter = $class::getDefaultFilter();
                     $collection = $filter->execute();
                     $response = array();
                     foreach($collection->getAll() as $object) {
@@ -147,7 +197,7 @@ class Api {
                 if ($result) self::respondAndDie (200, json_encode(true));
                 else self::respondAndDie (500, json_encode(false));
             default:
-                self::respondAndDie(405, 'Cannot handle request method: '.$_SERVER['REQUEST_METHOD']);
+                self::respondAndDie(405, 'Cannot handle request method: '.$method);
         }
         self::respondAndDie(500, 'Unspecified error - Ran through!');
     }
@@ -157,7 +207,7 @@ class Api {
      * @param int $code HTTP response code
      * @param string $message Content to transmit
      */
-    private static function respondAndDie($code, $message) {
+    protected static function respondAndDie($code, $message) {
         Errorhandler::checkParams($code, 'int', $message, 'string');
         http_response_code($code);
         echo $message;
@@ -177,7 +227,7 @@ class Api {
      * @param boolean $check_for_required_fields If true, then all required fields must be provided.
      * @return array|boolean True or an array of error messages.
      */
-    private static function updateObject($class, $object, $newdata, $check_for_required_fields = false) {
+    protected static function updateObject($class, $object, $newdata, $check_for_required_fields = false) {
         Errorhandler::checkParams($class, 'string', $object, '\\Platform\\Datarecord', $newdata, 'array', $check_for_required_fields, 'boolean');
         $structure = $class::getStructure();
         $errors = array();
@@ -228,18 +278,70 @@ class Api {
                     $object->setValue($key, $value);
                     break;
                 case Datarecord::FIELDTYPE_ENUMERATION:
-                    if (isset($structure[$key]['enumeration'][$value])) {
+                    if (! isset($structure[$key]['enumeration'][$value])) {
                         $errors[] = $value.' isn\'t a valid value for field '.$key;
                         continue;
                     }
                     $object->setValue($key, $value);
                     break;
+                case Datarecord::FIELDTYPE_IMAGE:
                 case Datarecord::FIELDTYPE_FILE:
-                    $file = new File();
-                    $file->loadForRead($value);
-                    if (! $file->isInDatabase()) {
-                        $errors[] = $value.' isn\'t a valid reference in field '.$key;
-                        continue;
+                    if (is_array($value)) {
+                        if ($value['action'] == 'remove') {
+                            // Build compatible input array
+                            $value = array(
+                                'status' => 'removed'
+                            );
+                        } else {
+                            // Check for all components
+                            $required = array('filename', 'mimetype', 'binary', 'action');
+                            foreach ($required as $r) {
+                                if (! isset($value[$r])) {
+                                    $errors[] = 'Required field '.$r.' missing when uploading file in field '.$key;
+                                    continue 2;
+                                }
+                            }
+                            
+                            // Check for valid action
+                            if ($value['action'] != 'add') {
+                                $errors[] = 'action should be either "add" or "remove" when uploading file in field '.$key;
+                                continue;
+                            }
+                            
+                            // Check for safe base64 decode
+                            $binary_data = base64_decode($value['binary'], true);
+                            if ($binary_data === false) {
+                                $errors[] = 'File content couldn\'t be BASE64-decoded in field '.$key;
+                                continue;
+                            }
+
+                            // Check for valid mime-type
+                            if ($structure[$key]['fieldtype'] == Datarecord::FIELDTYPE_IMAGE && strpos(strtolower($value['mimetype']),'image') === false) {
+                                $errors[] = 'Cannot add non-image mimetype to an image field in field '.$key;
+                                continue;
+                            }
+
+                            // Store binary content in temporary file
+                            $temp_file_name = \Platform\File::getTempFilename();
+                            $fh = fopen($temp_file_name, 'w');
+                            if (! $fh) $this->respondAndDie(500, 'Error writing temporary file handling field '.$key);
+                            fwrite($fh, $binary_data);
+                            fclose($fh);
+                            // Build compatible input array
+                            $value = array(
+                                'original_file' => $value['filename'],
+                                'mimetype' => $value['mimetype'],
+                                'temp_file' => substr($temp_file_name,strrpos($temp_file_name,'/')+1),
+                                'status' => 'changed'
+                            );
+                        }
+                    } else {
+                        $file = new File();
+                        $file->loadForRead($value);
+                        if (! $file->isInDatabase()) {
+                            $errors[] = $value.' isn\'t a valid reference in field '.$key;
+                            continue;
+                        }
                     }
                     $object->setValue($key, $value);
                     break;
