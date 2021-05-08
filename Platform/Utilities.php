7ca5ed56b17c0ce3c51b82c07006b6688ffab6dd
Michael Sahl <ms@ventu.dk>
2021-05-08 14:40:13 +0200

    Added function to translate physical path to URL

--- Platform/Utilities.php
+++ Platform/Utilities.php
@@ -11,4 +11,17 @@ class Utilities {
     public static function arrayRemove(array &$array, $element) {
         $array = array_diff($array, array($element));
     }
+    
+    /**
+     * Translates a physical path to an URL. Only works when script is called from
+     * web server
+     * @param string $directory Directory to transform
+     * @return string Corresponding URL
+     */
+    public static function directoryToURL(string $directory) {
+        $url = substr($directory, strlen($_SERVER['DOCUMENT_ROOT']));
+        if (substr($url,0,1) != '/') $url = '/'.$url;
+        if (substr($url,-1) != '/') $url .= '/';
+        return $url;
+    }
 }
