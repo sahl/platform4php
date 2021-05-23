09f7c65c737a91de74ca8a552577a67f0c431760
Michael Sahl <ms@ventu.dk>
2021-05-23 23:48:32 +0200

    Moved dialog form out of the properties

--- Platform/UI/Dialog.php
+++ Platform/UI/Dialog.php
@@ -3,12 +3,21 @@ namespace Platform\UI;
 
 class Dialog extends Component {
     
+    public $form = null;
+    
     protected $properties = [
         'title' => '',
-        'text' => '',
-        'form' => null
+        'text' => ''
     ];
     
+    /**
+     * Create a standard dialog
+     * @param string $id Id of dialog
+     * @param string $title Title of dialog
+     * @param string $text Text in dialog
+     * @param array $buttons Buttons as button text hashed by events which they trigger. "close" event closes the dialog
+     * @param \Platform\Form $form Form to display
+     */
     public function __construct(string $id, string $title, string $text, array $buttons = array(), $form = false) {
         parent::__construct();
         $this->addClass('platform_invisible');
