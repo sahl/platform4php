569935bee9922e49074516031ed0a8054c028df9
Michael Sahl <ms@ventu.dk>
2022-02-19 15:08:36 +0100

    Added height on multicheckboxfield

--- Platform/Form/MulticheckboxField.php
+++ Platform/Form/MulticheckboxField.php
@@ -3,7 +3,13 @@ namespace Platform\Form;
 
 class MulticheckboxField extends Field {
     
+    private $height;
+    
     public function __construct(string $label, string $name, array $options = array()) {
+        if ($options['height']) {
+            $this->height = $options['height'];
+            unset($options['height']);
+        }
         parent::__construct($label, $name, $options);
         $this->classes[] = '';
     }
@@ -16,7 +22,9 @@ class MulticheckboxField extends Field {
     
     public function renderInput() {
         if (! $this->value) $this->value = array();
-        echo '<div class="multi_checkbox_container" id="'.$this->getFieldIdForHTML().'">';
+        $style = '';
+        if ($this->height) $style = 'max-height: '.$this->height.'px; overflow: auto; padding: 3px;';
+        echo '<div class="multi_checkbox_container" id="'.$this->getFieldIdForHTML().'" style="'.$style.'">';
         foreach ($this->options as $key => $option) {
             $checked = in_array($key, $this->value) ? ' checked' : '';
             echo '<input class="'.$this->getClassString().'" style="vertical-align: -1px;" type="checkbox" name="'.$this->name.'[]" value="'.$key.'"'.$this->additional_attributes.$checked.'> '.$option.'<br>';
