89e78c4f04763bde3fcf8952871d4654c273f3bc
Michael Sahl <ms@ventu.dk>
2022-11-06 20:45:20 +0100

    The combobox is no longer protected by an accesstoken

--- Platform/Form/php/io_combobox.php
+++ Platform/Form/php/io_combobox.php
@@ -1,10 +1,6 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/Platform/include.php';
 
-use Platform\Security\Accesstoken;
-
-if (! Accesstoken::validateSession()) die();
-
 if (!class_exists($_GET['class'])) { $output = array(); }
 else $output = $_GET['class']::findByKeywords($_GET['term'], 'autocomplete');
 
