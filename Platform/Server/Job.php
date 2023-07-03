8309874e3530fd236416edfbd3922ce3a5c973e1
Michael Sahl <ms@ventu.dk>
2023-07-03 13:52:16 +0200

    More fix joblogging

--- Platform/Server/Job.php
+++ Platform/Server/Job.php
@@ -398,7 +398,7 @@ class Job extends \Platform\Datarecord {
             foreach ($running_jobs as $running_job) {
                 if ($running_job->isRunning()) {
                     if ($running_job->isOverdue()) {
-                        $running_job->log('overdue', 'Job have exceeded '.$this->max_runtime.' minutes runtime.', $this);
+                        $running_job->log('overdue', 'Job have exceeded '.$running_job->max_runtime.' minutes runtime.', $running_job);
                         $running_job->kill();
                         $running_job->cleanUp();
                     } else {
