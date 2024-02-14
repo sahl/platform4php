a00506a3c01891c531d9aad0f1ff1569642dfbba
Michael Sahl <ms@ventu.dk>
2024-02-14 14:09:03 +0100

    A non-checked checkbox is now considered empty

--- Platform/Form/js/CheckboxField.js
+++ Platform/Form/js/CheckboxField.js
@@ -12,6 +12,10 @@ Platform.Form.CheckboxField = class extends Platform.Form.Field {
         this.dom_node.find('input[type="checkbox"]').prop('checked', value == 1);
     }
     
+    isEmpty() {
+        return ! this.dom_node.find('input[type="checkbox"]').is(':checked');
+    }
+    
 }
 
 Platform.Component.bindClass('platform_component_checkbox_field', Platform.Form.CheckboxField);
