b5b7d8ec82096097e07ffaef9fb1fbc00731bc40
Michael Sahl <ms@ventu.dk>
2024-04-15 08:47:57 +0200

    FileField can now detect isEmpty correct

--- Platform/Form/js/FileField.js
+++ Platform/Form/js/FileField.js
@@ -3,14 +3,22 @@ Platform.Form.FileField = class extends Platform.Form.Field {
     clear() {
         var dom_node = this.dom_node.find('iframe');
         dom_node.prop('src', '/Platform/Form/php/file.php?form_name='+dom_node.closest('form').attr('id')+'&field_name='+dom_node.data('name')+'&file_id=');
+        this.trigger('change');
     }
 
     getValue() {
     }
+    
+    isEmpty() { 
+        var name = this.dom_node.attr('data-field_name'); 
+        var temp_file = this.dom_node.find('input[name="'+name+'[temp_file]"]').val(); 
+        return temp_file === ''; 
+    } 
 
     setValue(value) {
         var dom_node = this.dom_node.find('iframe');
         dom_node.prop('src', '/Platform/Form/php/file.php?form_name='+dom_node.closest('form').attr('id')+'&field_name='+dom_node.data('name')+'&file_id='+value);
+        this.trigger('change');
    }
     
 }
