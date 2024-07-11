6120a6373f729a7302b87c0755f510d79462faf4
Michael Sahl <ms@ventu.dk>
2024-07-11 13:44:11 +0200

    Select field with categories

--- Platform/Form/SelectField.php
+++ Platform/Form/SelectField.php
@@ -17,12 +17,40 @@ class SelectField extends Field {
     }
     
     public function renderInput() {
-        echo '<select data-fieldclass="'.$this->getFieldClass().'" class="'.$this->getFieldClasses().'" style="max-width: '.$this->field_width.';" name="'.$this->name.'" id="'.$this->getFieldIdForHTML().'"'.$this->additional_attributes.'>';
-        echo '<option style="background: white; colour:black;" value="">'.$this->heading;
+        $attributes = ['data-fieldclass' => $this->getFieldClass(),
+                       'class' => $this->getFieldClasses(),
+                       'style' => 'max-width: '.$this->field_width.';',
+                       'name' => $this->name,
+                       'id' => $this->getFieldIdForHTML()
+                       ];
+        foreach ($attributes as $name => $value)    $attributes[$name] = $name . '= "' . $value . '"';
+        
+        /**
+         * The options list can be either a array of title hashed by fieldname, or it can be an array
+         * of subarrays hashed by group names, in which case each subarray is title hashed by fieldname
+         */
+        echo '<select '.implode(' ', $attributes).'>';
+        echo   '<option style="background: white; colour:black;" value="">'.$this->heading;
         foreach ($this->options as $key => $option) {
-            $selected = $key == $this->value ? ' selected' : '';
-            $style = (array_key_exists($key, $this->options_colours) && $this->options_colours[$key]) ? 'background: '.$this->options_colours[$key].'; color:'.\Platform\Utilities\Utilities::getContrastColour($this->options_colours[$key]).';' : 'background: white; color: black;';
-            echo '<option style="'.$style.'" value="'.htmlentities($key, ENT_QUOTES).'"'.$selected.'>'.$option;
+            if (is_array($option)) {
+                $group = $key;
+                if ($group)   echo '<optgroup label="'.$group.'">';
+                $options = $option;
+                foreach ($options as $key => $option) {
+                    $selected = $key == $this->value ? ' selected' : '';
+                    $style = 'background: white; color: black;';
+                    if (array_key_exists($key, $this->options_colours) && $this->options_colours[$key])
+                        $style = 'background: '.$this->options_colours[$key].'; color:'.\Platform\Utilities\Utilities::getContrastColour($this->options_colours[$key]).';';
+                    echo '<option style="'.$style.'" value="'.htmlentities($key, ENT_QUOTES).'"'.$selected.'>'.$option;
+                }
+                if ($group)   echo '</optgroup>';
+            } else {
+                $selected = $key == $this->value ? ' selected' : '';
+                $style = 'background: white; color: black;';
+                if (array_key_exists($key, $this->options_colours) && $this->options_colours[$key])
+                    $style = 'background: '.$this->options_colours[$key].'; color:'.\Platform\Utilities\Utilities::getContrastColour($this->options_colours[$key]).';';
+                echo '<option style="'.$style.'" value="'.htmlentities($key, ENT_QUOTES).'"'.$selected.'>'.$option;
+            }
         }
         echo '</select>';
     }
