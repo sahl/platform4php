54480c35b1311e5ff082af0400e31ef6de2ff78f
Michael Sahl <ms@ventu.dk>
2025-02-13 11:39:36 +0100

    Changed the log format as the used format was generally stupid

--- Platform/Utilities/Log.php
+++ Platform/Utilities/Log.php
@@ -54,27 +54,19 @@ class Log {
      */
     public function log() {
         umask(002);
-        // Sort log items into columns
-        $columns = array();
-        $columns[0][-1] = date('H:i:s');
-        for ($i = 0; $i < func_num_args(); $i++) {
-            $full_text = func_get_arg($i);
-            if ($i < count($this->lineformat)) $full_text = wordwrap($full_text, $this->lineformat[$i]-1, "\n", true);
-            $j = 0;
-            foreach (explode("\n", $full_text) as $line) $columns[$j++][$i] = $line;
-        }
         // Render to file
         $fh = fopen($this->logdir.'/'.date('Y-m-d').'-'.$this->logname.'.log', 'a');
         if ($fh === false) trigger_error('Could not open logfile '.$this->logdir.'/'.date('Y-m-d').'-'.$this->logname.'.log for writing', E_USER_ERROR);
-        foreach ($columns as $line) {
-            for ($i = -1; $i < func_num_args(); $i++) {
-                $string = $line[$i];
-                if ($i == -1) $output_line = str_pad($string,9,' ').' ';
-                elseif ($i >= count($this->lineformat)) $output_line .= ' '.$string;
-                else $output_line .= ' '.str_pad($string, $this->lineformat[$i], ' ', $this->linealign[$i] == 'r' ? STR_PAD_LEFT : STR_PAD_RIGHT);
+        $output_line = date('Y-m-d H:i:s').'.'.str_pad(microtime(true)*1000%1000,3,'0', STR_PAD_LEFT);
+        for ($i = 0; $i < func_num_args(); $i++) {
+            $string = func_get_arg($i);
+            if ($i < count($this->lineformat)) {
+                $output_line .= ' '.str_pad($string, $this->lineformat[$i], ' ', $this->linealign[$i] == 'r' ? STR_PAD_LEFT : STR_PAD_RIGHT);
+            } else {
+                $output_line .= ' '.$string;
             }
-            fwrite($fh, $output_line."\n");
         }
+        fwrite($fh, str_replace(["\r", "\n"], ["", "\\n"], $output_line)."\n");
         fclose($fh);
     }
     
