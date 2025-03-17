6ae802117f7feaf2db53dce3b864362f0ef1b382
Michael Sahl <ms@ventu.dk>
2025-03-17 13:56:24 +0100

    New and better Chart component

---
+++ Platform/UI/Chart.php
@@ -0,0 +1,106 @@
+<?php
+namespace Platform\UI;
+/**
+ * Component class for drawing charts using Google Charts
+ * 
+ * @link https://wiki.platform4php.dk/doku.php?id=chart_class
+ */
+
+use Platform\UI\Component;
+use Platform\Utilities\Utilities;
+
+class Chart extends Component {
+    
+    protected static $component_class = 'platform_chart';
+    
+    const CHART_TYPE_BAR = 1;
+    const CHART_TYPE_COLUMN = 2;
+    const CHART_TYPE_PIE = 3;
+    const CHART_TYPE_LINE = 4;
+    
+    const CHART_TYPE_AREA = 10;
+    const CHART_TYPE_SCATTER = 11;
+    const CHART_TYPE_STEPPED_AREA = 12;
+
+    const CHART_TYPE_HISTOGRAM = 15;
+    
+    const CHART_TYPE_BUBBLE = 20;
+    const CHART_TYPE_CANDLESTICK = 21;
+    const CHART_TYPE_GAUGE = 22;
+    const CHART_TYPE_GEO = 23;
+    const CHART_TYPE_TIMELINE = 24;
+
+    const CHART_TYPE_COMBO = 100;
+    
+
+    protected $chart_data = [];
+    
+    protected $chart_options = [];
+    
+    public function __construct() {
+        parent::__construct();
+        self::JSFile(Utilities::directoryToURL(__DIR__).'js/Chart.js');
+        $this->addPropertyMap(
+                ['chart_type' => self::CHART_TYPE_LINE, 'column_types' => []]
+        );
+    }
+    
+    public function getCommonOptions() : array {
+        return [
+            'title' => 'Chart title',
+            'colors' => ['red', 'yellow', 'green'],
+            'is3D' => true,
+            'vAxis' => ['format' => 'long', 'gridlines' => 3],
+            'legend' => ['position' => 'bottom'],
+        ];
+    }
+    
+    public function clearData() {
+        $this->chart_data = [];
+    }
+    
+    public function addRow(array $row) {
+        $this->chart_data[] = $row;
+    }
+    
+    public function addIndexedData(array $data_rows, array $label_info) {
+        // Clear existing data
+        $this->clearData();
+        
+        // Add a row with the data sets
+        $label_row = ['Label'];
+        foreach ($label_info as $label) $label_row[] = $label;
+        $this->addRow($label_row);
+        
+        // Now add data ensuring we only include labeled data (and in the correct order)
+        foreach ($data_rows as $label => $data) {
+            $row = [$label];
+            foreach ($label_info as $key => $label) {
+                $row[] = array_key_exists($key, $data) ? $data[$key] : null;
+            }
+            $this->addRow($row);
+        }
+    }
+    
+    protected function prepareData() {
+        parent::prepareData();
+        $this->addData('chart_data', json_encode($this->chart_data));
+        $this->addData('options', json_encode($this->chart_options));
+    }
+    
+    public function setChartOption(string $option, $value) {
+        $this->chart_options[$option] = $value;
+    }
+    
+    public function setChartOptions(array $options) {
+        foreach ($options as $option => $value)
+            $this->setChartOption ($option, $value);
+    }
+    
+    public function setColumnType(int $column, $type = null) {
+        $column_types = $this->column_types;
+        if ($type === null) unset($column_types[$column]);
+        else $column_types[$column] = $type;
+        $this->column_types = $column_types;
+    }
+}
---
+++ Platform/UI/js/Chart.js
@@ -0,0 +1,81 @@
+google.charts.load('current', {'packages':['corechart', 'gauge', 'geochart', 'timeline'], 'language' : navigator.language});
+
+Platform.Chart = class extends Platform.Component {
+    
+    chart = null
+
+    initialize() {
+        var component = this;
+        google.charts.setOnLoadCallback(function() {
+            switch (component.getProperty('chart_type')) {
+                case 2:
+                    component.chart = new google.visualization.ColumnChart(component.dom_node.get(0));
+                    break;
+                case 3:
+                    component.chart = new google.visualization.PieChart(component.dom_node.get(0));
+                    break;
+                case 4:
+                    component.chart = new google.visualization.LineChart(component.dom_node.get(0));
+                    break;
+                case 10:
+                    component.chart = new google.visualization.AreaChart(component.dom_node.get(0));
+                    break;
+                case 11:
+                    component.chart = new google.visualization.ScatterChart(component.dom_node.get(0));
+                    break;
+                case 12:
+                    component.chart = new google.visualization.SteppedAreaChart(component.dom_node.get(0));
+                    break;
+                case 15:
+                    component.chart = new google.visualization.Histogram(component.dom_node.get(0));
+                    break;
+                case 20:
+                    component.chart = new google.visualization.BubbleChart(component.dom_node.get(0));
+                    break;
+                case 21:
+                    component.chart = new google.visualization.CandlestickChart(component.dom_node.get(0));
+                    break;
+                case 22:
+                    component.chart = new google.visualization.Gauge(component.dom_node.get(0));
+                    break;
+                case 23:
+                    component.chart = new google.visualization.GeoChart(component.dom_node.get(0));
+                    break;
+                case 24:
+                    component.chart = new google.visualization.Timeline(component.dom_node.get(0));
+                    break;
+                case 100:
+                    component.chart = new google.visualization.ComboChart(component.dom_node.get(0));
+                    break;
+                default:
+                    component.chart = new google.visualization.BarChart(component.dom_node.get(0));
+                    break;
+            }
+            component.handle_chart();
+        });
+    }
+    
+    handle_chart() {
+        var component = this;
+        var data = component.dom_node.data('chart_data');
+        var column_types = component.getProperty('column_types');
+        console.log(column_types);
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[i].length; j++) {
+                switch (column_types[j]) {
+                    case 'date':
+                        if (data[i][j]) data[i][j] = new Date(data[i][j]);
+                        break;
+                }
+            }
+        }
+
+       var chart_data = google.visualization.arrayToDataTable(data);
+       var options = component.dom_node.data('options');
+       if (Array.isArray(options)) options = {};
+
+       component.chart.draw(chart_data, options);
+    }
+}
+
+Platform.Component.bindClass('platform_chart', Platform.Chart);
