43b3010ca5096e9ccd6349792e3f951ff1c520e8
Michael Sahl <ms@ventu.dk>
2026-07-02 20:33:02 +0200

    Fixed wrong string concatenator

--- Platform/Datarecord/Collection.php
+++ Platform/Datarecord/Collection.php
@@ -100,7 +100,7 @@ class Collection implements Iterator,Countable {
      * @return Datarecord
      */
     public function get(int $i) {
-        if ($i < 0 || $i >= $this->getCount()) trigger_error('The value '+$i+' is not a valid index in the collection', E_USER_ERROR);
+        if ($i < 0 || $i >= $this->getCount()) trigger_error('The value '.$i.' is not a valid index in the collection', E_USER_ERROR);
         return $this->datarecords[$i];
     }
 
@@ -298,7 +298,7 @@ class Collection implements Iterator,Countable {
      * @param int $i The index of the element. Higher indexes will be shifted down.
      */
     public function remove(int $i) {
-        if ($i < 0 || $i >= $this->getCount()) trigger_error('The value '+$i+' is not a valid index in the collection', E_USER_ERROR);
+        if ($i < 0 || $i >= $this->getCount()) trigger_error('The value '.$i.' is not a valid index in the collection', E_USER_ERROR);
         array_splice($this->datarecords,$i,1);
     }
 
